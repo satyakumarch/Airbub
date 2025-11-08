9709735d68ba40f154fc3e8d37595509e21d9975
SatyaKumar <satyakumarchaudhary603@gmail.com>
2025-11-09 00:57:45 +0530

    completed listing.model.js file

---
+++ Backend/models/listing.model.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";    
+import { User } from "./user.model.js";
+const listingSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    host: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    image1: {
+        type: String,
+        required: true
+    },
+    image2: {
+        type: String,
+        required: true
+    },
+    rent: {
+        type: Number,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    landmark: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    isBooked: {
+        type: Boolean,
+        default: false
+    }
+},{ timestamps: true });
+ 
+
+const Listing = mongoose.model('Listing', listingSchema);
+export default Listing;
